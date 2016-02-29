4eb826c94ef3663532ed28ab0b3cc336231d6287
maximri <maximri@wix.com>
2016-02-29 18:54:24 +0200

    changing direction field in MessageEmail to be a JsonScalaEnumeration

--- hive-model/src/main/scala/com/wix/hive/model/activities/MessagesEmail.scala
+++ hive-model/src/main/scala/com/wix/hive/model/activities/MessagesEmail.scala
@@ -1,7 +1,11 @@
 package com.wix.hive.model.activities
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.module.scala.JsonScalaEnumeration
 import com.wix.hive.model.activities.ActivityType._
+
+import com.wix.hive.model.activities.Direction.Direction
 
 case class MessagesEmail(content: Content,
                          messageId: String,
@@ -11,10 +15,18 @@ case class MessagesEmail(content: Content,
                          cc: Seq[String],
                          subject: Option[String],
                          replyTo: Option[String],
-                         direction: Enumeration,
+                         @JsonScalaEnumeration(classOf[DirectionRef])direction: Direction,
                          metadata: Seq[ItemMetadata]) extends ActivityInfo {
   @JsonIgnore
   override def activityType: ActivityType = `messaging/email`
 }
 
 case class Content(message: String, media: Seq[String])
+
+class DirectionRef extends TypeReference[Direction.type]
+
+object Direction extends Enumeration {
+  type Direction = Value
+  val BusinessToCustomer = Value("BUSINESS_TO_CUSTOMER")
+  val CustomerToBusiness = Value("CUSTOMER_TO_BUSINESS")
+}
